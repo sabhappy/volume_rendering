7127a3058ae600113d6944e90a522b9997f2bf34
sabhappy <sabhappy@web.de>
2017-05-10 18:15:52 +0200

    reading *raw data + creating 3D Texture + render 2D slice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,122 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <QCoreApplication>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+
+void writeFile()
+{
+    std::ofstream myfile;
+    myfile.open ("/Users/sabrinaheppner/Documents/workspace_Qt/readData/example2.txt");
+    if (myfile.is_open())
+    {
+       // myfile << "This is my new file. \nThis is a line.\n";
+       // myfile << "This is another line.\n";
+        myfile << "Test";
+        std::cout << "writing into file " << std::endl;
+        myfile.close();
+    }else{
+        std::cerr << "NOT writing into file " << std::endl;
+    }
+}
+
+void readFile()
+{
+
+    std::string line;
+    std::ifstream myfile ("/Users/sabrinaheppner/Documents/workspace_Qt/readData/data.txt");
+     if (myfile.is_open())
+     {
+       std::cout << "reading from file data.txt:\n" << std::endl;
+       while (std::getline (myfile,line) )
+       {
+         std::cout << line << '\n';
+       }
+       myfile.close();
+       std::cout<< std::endl;
+     }
+
+     else std::cout << "Unable to open file";
+}
+
+char* readByteFile()
+{
+    FILE *fileptr;
+    char *buffer;
+    long filelen;
+
+    fileptr = fopen("/Users/sabrinaheppner/Documents/workspace_Qt/readData/heart.raw", "rb");  // Open the file in binary mode
+    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+    filelen = ftell(fileptr);             // Get the current byte offset in the file
+
+    std::cout << "byte offset in the file:"<<filelen << "\n"<< std::endl;
+
+    rewind(fileptr);                      // Jump back to the beginning of the file
+
+    buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
+    fread(buffer, filelen, 1, fileptr); // Read in the entire file
+
+    //std::cout << "file data:"<< buffer << "\n"<< std::endl;
+
+    fclose(fileptr); // Close the file
+
+    return buffer;
+}
+
+void getFileSize()
+{
+    std::streampos begin,end;
+    std::ifstream myfile ("/Users/sabrinaheppner/Documents/workspace_Qt/readData/data.txt");
+    if (myfile.is_open())
+    {
+        begin = myfile.tellg();
+        myfile.seekg (0, std::ios::end);
+        end = myfile.tellg();
+        myfile.close();
+        std::cout << "size is: " << (end-begin) << " bytes.\n"<<std::endl;
+    }
+    else std::cout << "Unable to open file"<<std::endl;
+
+
+
+}
+
+
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QCoreApplication a(argc, argv);
+
+    writeFile();
+   // readFile();
+    //getFileSize();
+    char *buffer = readByteFile();
+    //std::cout << "file data:"<< buffer << "\n"<< std::endl;
+    int s = sizeof(buffer);
+    std::cout << "size:"<< s << "\n"<< std::endl;
+
+    int XDIM = 64;
+    int YDIM = 64;
+    int ZDIM = 64;
+
+    char img[XDIM][YDIM][ZDIM];
+
+
+    for (int x = 0; x < XDIM; x++) {
+        for (int y = 0; y < YDIM; y++) {
+            char c = buffer[(x + (XDIM * y) + (5 * XDIM * YDIM))];
+            img[x][y][0] = c;
+            img[x][y][1] = c;
+            img[x][y][2] = c;
+            img[x][y][3] = c;
+
+            std::cout << "c:"<< c << "\n"<< std::endl;
+        }
+    }
+
+
+
 
     return a.exec();
 }
--- readData.pro
+++ readData.pro
@@ -1,18 +1,18 @@
-#-------------------------------------------------
-#
-# Project created by QtCreator 2017-05-04T11:38:30
-#
-#-------------------------------------------------
+QT += core
+QT -= gui
 
-QT       += core gui opengl
+CONFIG += c++11
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+TARGET = readData
+CONFIG += console
+CONFIG -= app_bundle
 
-TARGET = test
 TEMPLATE = app
 
+SOURCES += main.cpp
+
 # The following define makes your compiler emit warnings if you use
-# any feature of Qt which as been marked as deprecated (the exact warnings
+# any feature of Qt which as been marked deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
 DEFINES += QT_DEPRECATED_WARNINGS
@@ -21,19 +21,3 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
-
-
-SOURCES += main.cpp\
-        mainwindow.cpp \
-    glwidget.cpp \
-    stb_image.c
-
-HEADERS  += mainwindow.h \
-    glwidget.h \
-    stb_image.h
-
-FORMS    += mainwindow.ui
-
-DISTFILES += \
-    side1.png
-
